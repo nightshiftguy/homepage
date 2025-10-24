a2171034c5ff3002b3d5dd88e32969059b2af2e6
Jan <jas.gol@poczta.onet.pl>
2025-10-24 11:40:14 +0200

    Install image loader,run npm update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
+import testImgSrc from "./img/profile_picture.jpg";
 
 const container = document.querySelector(".container");
 const text = document.createElement("p");
+const testImg = document.createElement("img");
+testImg.src = testImgSrc;
 text.textContent = "Hello World!";
 container.appendChild(text);
+container.appendChild(testImg);
