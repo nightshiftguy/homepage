058ac2f89b12140eb9b226f1a9b03ba66936bd2f
Jan <jas.gol@poczta.onet.pl>
2025-10-24 13:17:07 +0200

    Add assets (images and icons)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import "./styles.css";
-import testImgSrc from "./img/profile_picture.jpg";
+import testImgSrc from "./img/main-img.jpg";
 
 const container = document.querySelector(".container");
+const title = document.createElement("h1");
+title.textContent = "Title text";
 const text = document.createElement("p");
 const testImg = document.createElement("img");
 testImg.src = testImgSrc;
+testImg.setAttribute("height","400px")
 text.textContent = "Hello World!";
+container.appendChild(title)
 container.appendChild(text);
 container.appendChild(testImg);
